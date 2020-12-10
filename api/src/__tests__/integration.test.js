30bc3f432bc2819354940a25b29013a625d7d738
Andreske02 <andres.vergauwen@hotmail.com>
2020-12-10 14:03:46 +0100

    Revert "test(integration): test fix"
    This reverts commit e78b617e25119c746f6d005a89e0ca6e8ac06b82.

--- api/src/__tests__/integration.test.js
+++ api/src/__tests__/integration.test.js
@@ -23,11 +23,22 @@ let randomNumber = Math.floor(Math.random() * 10) + 1;
 
 request = supertest(app)
 // TEST
-describe('test', ()=>{
-    test('check if test is test', async (done) =>{
+describe('GET /test endpoint', ()=>{
+    test('check if response is 200', async (done) =>{
         try {
-            const response = 404;
-            expect(response).toBe(404,done())
+            const response = await request.get('/')
+            // expect(response.status).toBe(200,done())
+            expect(response.body).toStrictEqual({},done())
+        } catch (error) {
+            console.log(error);
+        }
+    })
+})
+describe('POST /test endpoint', ()=>{
+    test('check if response is 404', async (done) =>{
+        try {
+            const response = await request.post('/')
+            expect(response.status).toBe(404,done())
         } catch (error) {
             if (error) {
                 console.log(error);
