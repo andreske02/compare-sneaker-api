0ea359a86b71cf60dcba43ccdda31a6b0131cc3d
Andreske02 <andres.vergauwen@hotmail.com>
2021-01-02 19:22:56 +0100

    refactor(database): cascade sneakers on delete/update brand

--- api/src/utils/database.js
+++ api/src/utils/database.js
@@ -1,49 +1,22 @@
 const Helpers = require("./helpers.js");
-const uuid = Helpers.generateUUID();
- const pg = require("knex")({
-    client: "pg",
-    version: "9.6",
-    searchPath: ["knex", "public"],
-    connection: process.env.PG_CONNECTION_STRING
-      ? process.env.PG_CONNECTION_STRING
-      : "postgres://example:example@localhost:5432/sneakerdb",
-  });
+const pg = require("knex")({
+  client: "pg",
+  version: "9.6",
+  searchPath: ["knex", "public"],
+  connection: process.env.PG_CONNECTION_STRING
+    ? process.env.PG_CONNECTION_STRING
+    : "postgres://example:example@localhost:5432/sneakerdb",
+});
 
 const database = {
- 
   /*--------- INITIALIZE TABLES --------*/
   initialiseTables: async () => {
-    await pg.schema.hasTable("sneakers").then(async (exists) => {
-      if (!exists) {
-        await pg.schema
-          .createTable("sneakers", (table) => {
-            table.increments();
-            table.string("product_brand");
-            table.string("product_name");
-            table.string("product_price");
-            table.string("product_sale_price");
-            table.boolean("product_sale");
-            table.text("product_description");
-            table.text("product_image");
-            table.text("product_available");
-            table.text("product_colors");
-            table.text("product_url");
-            table.string("product_shipping_info");
-            table.string("brand_uuid");
-            table.timestamps(true, true);
-          })
-          .then(async () => {
-            console.log("🎉", "created sneakers table");
-            database.sneakerSeeders();
-          });
-      }
-    });
     await pg.schema.hasTable("brands").then(async (exists) => {
       if (!exists) {
         await pg.schema
           .createTable("brands", (table) => {
             table.increments();
-            table.uuid("uuid");
+            table.uuid("uuid").notNullable().unique();
             table.string("brand_name");
             table.string("brand_reviews");
             table.text("brand_logo");
@@ -56,10 +29,37 @@ const database = {
           });
       }
     });
+    await pg.schema.hasTable("sneakers").then(async (exists) => {
+      if (!exists) {
+        await pg.schema
+          .createTable("sneakers", (table) => {
+            table.increments();
+            table.uuid("uuid").notNullable().unique();
+            table.string("product_brand");
+            table.string("product_name");
+            table.string("product_price");
+            table.string("product_sale_price");
+            table.boolean("product_sale");
+            table.text("product_description");
+            table.text("product_image");
+            table.text("product_available");
+            table.text("product_colors");
+            table.text("product_url");
+            table.string("product_shipping_info");
+            table.uuid("brand_uuid").unsigned().references("uuid").inTable("brands").onDelete("CASCADE").onUpdate("CASCADE").notNullable();
+            table.timestamps(true, true);
+          })
+          .then(async () => {
+            console.log("🎉", "created sneakers table");
+            database.sneakerSeeders();
+          });
+      }
+    });
   },
   /*--------- SEEDERS --------*/
   sneakerSeeders: async () => {
     const sneakerObj = {
+      uuid: Helpers.generateUUID(),
       product_brand: "Nike",
       product_name: "Air Force 1 '07",
       product_price: "€ 99,00",
@@ -80,7 +80,7 @@ const database = {
       product_url:
         "https://www.snipes.be/nl/p/nike-air_force_1_shadow-white%2Fwhite%2Fwhite-00013801761737.html",
       product_shipping_info: "8.7",
-      brand_uuid: uuid,
+      brand_uuid:"6fed38e0-4d1d-11eb-9764-7b26be27a53d",
     };
     const sneakers = await pg
       .table("sneakers")
@@ -93,39 +93,40 @@ const database = {
         console.log("💩", e);
       });
   },
-
-
-
-
-
   brandSeeders: async () => {
-    const brandObj = {
-      uuid: uuid,
-      brand_name: "Snipes",
-      brand_reviews: "8.7",
-      brand_logo:
-        "https://www.snipes.nl/on/demandware.static/Sites-snse-NL-BE-Site/-/default/dwcc537b29/images/snipes_logo.svg",
-      brand_url: "https://www.snipes.com/",
-    };
-    const brands = await pg
+    const brandArrayObj =[
+      {
+        uuid: Helpers.generateUUID(),
+        brand_name: "snipes",
+        brand_reviews: "8.7",
+        brand_logo:
+          "https://www.snipes.nl/on/demandware.static/Sites-snse-NL-BE-Site/-/default/dwcc537b29/images/snipes_logo.svg",
+        brand_url: "https://www.snipes.com/",
+      },
+      {
+        uuid: '6fed38e0-4d1d-11eb-9764-7b26be27a53d',
+        brand_name: "torfs",
+        brand_reviews: "8.7",
+        brand_logo:
+          "https://www.torfs.be/on/demandware.static/Sites-Torfs-Webshop-BE-Site/-/default/dw1f8272ff/images/logo_standard.svg",
+        brand_url: "https://www.torfs.be/",
+      },
+    ] 
+    for (const brandObj of brandArrayObj) {
+      const brands = await pg
       .table("brands")
       .insert(brandObj)
       .then(async function () {
         console.log("✅", "Created new brand");
-        return;
       })
       .catch((e) => {
         console.log("💩", e);
       });
+    }
+    
   },
 
-
-
-
-
-
-
-  
+  /*--------- CRUD  --------*/
   addSneakers: async (sneakerObj) => {
     const sneakers = await pg
       .table("sneakers")
@@ -138,6 +139,7 @@ const database = {
         console.log("💩", e);
       });
   },
+
   deleteSneakers: async () => {
     const sneakers = await pg
       .table("sneakers")
